c3541ee2be229ff0927a69443b1171a871b8e747
davidgress <david.e.gress@gmail.com>
2023-08-13 10:10:49 -0400

    Day 33: Filter the data

--- backend.py
+++ backend.py
@@ -2,14 +2,21 @@ import requests
 API_KEY = "8d94efc882da2259cf76fdce33c2be37"
 
 
-def get_data(place, forcast_days=None, kind=None):
+def get_data(place, forecast_days=None, kind=None):
     url = "http://api.openweathermap.org/data/2.5/forecast?" \
           f"q={place}&" \
           f"appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict1["main"]["temp"] for dict1 in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict1["weather"][0]["main"] for dict1 in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place='Tokyo'))
+    print(get_data(place='Tokyo', forecast_days=3, kind="Temperature"))
